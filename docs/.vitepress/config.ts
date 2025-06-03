82d03c83bba77bdd38c7c49bc4066cd56442f059
Chris <chrisbreuer93@gmail.com>
2025-06-02 19:39:57 -0700

    chore: wip

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -11,7 +11,7 @@ const analyticsHead: HeadConfig[] = [
     'script',
     {
       'src': 'https://cdn.usefathom.com/script.js',
-      'data-site': 'SULRKVYD',
+      'data-site': 'PPQCJNSG',
       'defer': '',
     },
   ],
